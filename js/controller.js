541302be98a7ba6673c2cb14bca782035e479c54
Martin Zelník <Martin.Zelnik@ibm.com>
2021-04-07 18:45:31 +0200

    category implemented in controller

--- js/controller.js
+++ js/controller.js
@@ -13,8 +13,8 @@
 		self.model = model;
 		self.view = view;
 
-		self.view.bind('newTodo', function (title) {
-			self.addItem(title);
+		self.view.bind('newTodo', function (title, category) {
+			self.addItem(title, category);
 		});
 
 		self.view.bind('itemEdit', function (item) {
@@ -92,14 +92,14 @@
 	 * An event to fire whenever you want to add an item. Simply pass in the event
 	 * object and it'll handle the DOM insertion and saving of the new item.
 	 */
-	Controller.prototype.addItem = function (title) {
+	Controller.prototype.addItem = function (title, category) {
 		var self = this;
 
-		if (title.trim() === '') {
+		if (title.trim() === '' || category.trim() === '') {
 			return;
 		}
 
-		self.model.create(title, function () {
+		self.model.create(title, category, function () {
 			self.view.render('clearNewTodo');
 			self._filter(true);
 		});
