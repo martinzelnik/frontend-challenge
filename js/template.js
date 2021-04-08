abe4230baa1c13e3b0a51247d828456727f45602
Martin Zelník <Martin.Zelnik@ibm.com>
2021-04-08 13:06:16 +0200

    naming change from category to categoryName

--- js/template.js
+++ js/template.js
@@ -74,7 +74,7 @@
 
 			template = template.replace('{{id}}', data[i].id);
 			template = template.replace('{{title}}', escape(data[i].title));
-			template = template.replace('{{category}}', escape(data[i].category));
+			template = template.replace('{{category}}', escape(data[i].categoryName));
 			template = template.replace('{{completed}}', completed);
 			template = template.replace('{{checked}}', checked);
 
